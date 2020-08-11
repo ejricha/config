cd612d0aa79b308aef7d21e4f1a77f56bdee37a1
Eric Richardson <ejricha+github@gmail.com>
2020-08-11 10:53:00 -0400

    Switch back to Ubuntu Mono font

--- .hyper.js
+++ .hyper.js
@@ -9,12 +9,13 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 14,
+    fontSize: 18,
 
     // font family with optional fallbacks
-    //fontFamily: 'Ubuntu Mono, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+	//fontFamily: 'Ubuntu Mono, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: 'Ubuntu Mono',
     //fontFamily: 'Source Code Pro',
-    fontFamily: 'MesloLGS NF',
+    //fontFamily: 'MesloLGS NF',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
