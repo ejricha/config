97fbcf295626564ede16b7717eb9bb8f5af90d07
Eric Richardson <ejricha+github@gmail.com>
2025-09-17 08:39:32 -0600

    install: kitty and neovim come from curl

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Install the following packages
-PACKAGES="git zsh tmux neovim ssh rsync curl htop"
+PACKAGES="git tmux zsh ssh rsync curl htop"
 DIR=`dirname $(realpath $0)`
 DATETIME=`date +%Y-%m-%d_%H%M%S`
-DIR_ZSH="~/.oh_my_zsh"
+DIR_ZSH="$HOME/.oh-my-zsh"
 DIR_ZSH_PLUGINS="$DIR_ZSH/custom/plugins"
+DIR_TMUX="$HOME/.tmux"
+DIR_TMUX_TPM="$HOME/.tmux/plugins/tpm"
+DIR_NVIM_CONFIG="$HOME/.config/nvim"
 
 # Main function
 main() {
@@ -20,43 +23,98 @@ install_packages() {
   sudo apt install $PACKAGES
   echo
 
+   
+  echo "[Install kitty]"
+  install_kitty
+  echo
+
+  echo "[Install neovim]"
+  install_neovim
+  echo
+
   echo "[Clone a few git repos]"
   clone_repo https://github.com/robbyrussell/oh-my-zsh.git $DIR_ZSH
   clone_repo https://github.com/zsh-users/zsh-syntax-highlighting.git $DIR_ZSH_PLUGINS/zsh-syntax-highlighting
   clone_repo https://github.com/Aloxaf/fzf-tab $DIR_ZSH_PLUGINS/fzf-tab
+  clone_repo https://github.com/tmux-plugins/tpm $DIR_TMUX_TPM
 
-  clone_repo https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   echo
+}
+
+install_kitty() {
+  # Check the current version
+  app="$HOME/.local/kitty.app/bin/kitty"
+  version=`$app -v 2>/dev/null | grep -oE "[0-9\.]+"`
+  echo "version is $version"
+  if [[ $version =~ "0.43." ]]
+  then
+    echo "great, already installed"
+    return
+  fi
+
+  # Only update if we need to
+  echo "too old, need to update"
+  installer="https://sw.kovidgoyal.net/kitty/installer.sh"
+  curl -L $installer | sh /dev/stdin
+  sudo ln -sf $app /usr/local/bin/
+}
+
+install_neovim() {
+  # Check the current version
+  target="nvim-linux-x86_64"
+  app="/opt/$target/bin/nvim"
+  version=`$app -v | head -n1 | grep -Eo "[0-9\.]+"`
+  echo "version is $version"
+  if [[ $version =~ "0.11." ]]
+  then
+    echo "great, already installed"
+    return
+  fi
+
+  # Only update if we need to
+  echo "too old, need to update"
+  tmp=`mktemp -d`
+  cd $tmp
+  installer="https://github.com/neovim/neovim/releases/latest/download/${target}.tar.gz"
+  curl -LO $installer
+  sudo rm -rf /opt/$target
+  sudo tar -C /opt -xzf ${target}.tar.gz
+  cd -
+  rm -rf $tmp
+  sudo ln -sf $app /usr/local/bin/
+  sudo ln -sf $app /etc/alternatives/vim
+  sudo ln -sf $app /etc/alternatives/vi
 }
 
 # Fix all links on the system
 fix_all_links() {
   echo "[git]"
-  create_link $DIR/git/.gitconfig ~/
-  create_link $DIR/git/.gitexclude ~/
+  create_link $DIR/git/.gitconfig $HOME/
+  create_link $DIR/git/.gitexclude $HOME/
   echo
 
   echo "[zsh]"
-  create_link $DIR/zsh/.zshrc ~/
-  create_link $DIR/zsh/.zsh_aliases ~/
-  create_link $DIR/zsh/.oh_my_zsh/custom/themes/* ~/.oh-my-zsh/custom/themes/
+  create_link $DIR/zsh/.zshrc $HOME/
+  create_link $DIR/zsh/.zsh_aliases $HOME/
+  create_link $DIR/zsh/.oh_my_zsh/custom/themes/gavin.zsh-theme $DIR_ZSH/custom/themes/
+  create_link $DIR/zsh/.oh_my_zsh/custom/themes/eric.zsh-theme $DIR_ZSH/custom/themes/
   echo
 
   echo "[tmux]"
-  create_link $DIR/tmux/.tmux.conf ~/
+  create_link $DIR/tmux/.tmux.conf $HOME/
   echo
 
   echo "[nvim]"
-  create_link $DIR/nvim/ ~/.config/nvim
+  create_link $DIR/nvim/ $DIR_NVIM_CONFIG/
   echo
 }
 
 # Check a single link, and back it up and fix it if it is not correct
 create_link() {
   to=$1
-  d=${2%/}
+  dir_from=${2%/}
   f=`basename $to`
-  from=$d/$f
+  from=$dir_from/$f
 
   # Check to see if we need to create the link
   if [[ -h "$from" ]] # Link
@@ -79,7 +137,8 @@ create_link() {
   fi
 
   # Make the link
-  ln -s $to $d/
+  mkdir -p $dir_from/
+  ln -s $to $dir_from/
   if [[ -h "$from" && `readlink "$from"` == $to ]]
   then
     echo "  \"$from\" linked to $to"
@@ -98,15 +157,15 @@ backup_file() {
 
 # Clone a given repo
 clone_repo() {
-  to=$1
-  from=$2
-  echo "  ($from to $to)"
-  if [[ -d "$from" ]]
+  repo=$1
+  local=$2
+  echo "($repo in $local)"
+  if [[ -d "$local" ]]
   then
-    (cd $from && git pull)
+    (cd $local && git pull)
   else
-    echo git clone $to $from
-    git clone $to $from
+    echo git clone $repo $local
+    git clone $repo $local
   fi
 }
 
