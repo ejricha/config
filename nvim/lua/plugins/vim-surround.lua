3dc64db548d860f9bfb8c577404f956c96e4b2cc
Eric Richardson <ejricha+github@gmail.com>
2025-11-24 17:01:11 -0800

    nvim: add plugins vim-surround and vim-swap

---
+++ nvim/lua/plugins/vim-surround.lua
@@ -0,0 +1,5 @@
+return {
+  "tpope/vim-surround",
+  branch = "master",
+  enabled = vim.g.enabled_plugins,
+}
---
+++ nvim/lua/plugins/vim-swap.lua
@@ -0,0 +1,5 @@
+return {
+  "machakann/vim-swap",
+  branch = "master",
+  enabled = vim.g.enabled_plugins,
+}
